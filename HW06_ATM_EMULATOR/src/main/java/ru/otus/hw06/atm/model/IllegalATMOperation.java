6125b7aedc78d3cbf00dc860c13b5b7c039d9786
Alex_Foully <alexander.narbayev@gmail.com>
2019-12-29 17:34:56 +0700

    HomeWork06 - ATM

--- HW06_ATM_EMULATOR/src/main/java/ru/otus/hw06/atm/model/IllegalATMOperation.java
+++ HW06_ATM_EMULATOR/src/main/java/ru/otus/hw06/atm/model/IllegalATMOperation.java
@@ -1,6 +1,6 @@
 package ru.otus.hw06.atm.model;
 
-public class IllegalATMOperation extends Exception {
+public class IllegalATMOperation extends ATMException {
     public IllegalATMOperation(long value) {
         super("Недопустимая операция для банкомата/ячейки для значения:\t" + value);
     }
--- HW06_ATM_EMULATOR/src/main/java/ru/otus/hw06/atm/model/InsufficientFundsException.java
+++ HW06_ATM_EMULATOR/src/main/java/ru/otus/hw06/atm/model/InsufficientFundsException.java
@@ -1,6 +1,6 @@
 package ru.otus.hw06.atm.model;
 
-public class InsufficientFundsException extends Exception {
+public class InsufficientFundsException extends ATMException {
     public InsufficientFundsException(long value) {
         super("Невозможно выдать требуемую сумму:\t" + value);
     }

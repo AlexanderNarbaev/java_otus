586149f96fb2144667dce1a02030968fd28c49c0
Alex_Foully <alexander.narbayev@gmail.com>
2020-01-19 20:45:03 +0700

    HomeWork08 - JSON Serializable

--- hw08_JSONSerializable/src/main/java/ru/otus/hw08/json/serialization/VunderVafelJSONSerializator.java
+++ hw08_JSONSerializable/src/main/java/ru/otus/hw08/json/serialization/VunderVafelJSONSerializator.java
@@ -1,10 +1,13 @@
 package ru.otus.hw08.json.serialization;
 
+import lombok.extern.log4j.Log4j2;
+
 import javax.json.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.*;
 
+@Log4j2
 public class VunderVafelJSONSerializator {
 
     /**
@@ -15,15 +18,15 @@ public class VunderVafelJSONSerializator {
      */
     public String serializeToJson(Object objectToConvert) throws JsonSerializationException {
         if (objectToConvert == null) {
-            throw new JsonSerializationException();
+            throw new JsonSerializationException("Object to serialize is null");
         }
         try {
             JsonValue jsonValue = getJsonValue(objectToConvert);
             return jsonValue != null ? jsonValue.toString() : "";
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("Error when try to serialize JSON", e);
         }
-        return "";
+        throw new JsonSerializationException("No JSON when serialization done");
     }
 
     private List<Field> getAllFields(Class clazz) {
@@ -36,7 +39,7 @@ public class VunderVafelJSONSerializator {
         return result;
     }
 
-    private JsonValue getJsonValue(Object objectToConvert) throws IllegalAccessException {
+    private JsonValue getJsonValue(Object objectToConvert) throws Exception {
         if (objectToConvert == null) return null;
         JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
         String primitiveValue = getPrimitiveValue(objectToConvert);
@@ -60,7 +63,7 @@ public class VunderVafelJSONSerializator {
                         }
                         objectBuilder.add(field.getName(), arrayBuilder);
                     }
-                } else if (field.getType().isInstance(Collection.class)) {
+                } else if (Collection.class.isAssignableFrom(field.getType())) {
                     JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
                     for (Object o : ((Collection) fieldValue)) {
                         arrayBuilder.add(getJsonValue(o));
@@ -79,22 +82,12 @@ public class VunderVafelJSONSerializator {
                 || primitive.getClass().equals(Boolean.class)) {
             return ((Boolean) primitive).toString();
         } else if (primitive.getClass().equals(byte.class)
-                || primitive.getClass().equals(Byte.class)) {
-            return ((Byte) primitive).toString();
-        } else if (primitive.getClass().equals(short.class)
-                || primitive.getClass().equals(Short.class)) {
-            return ((Short) primitive).toString();
-        } else if (primitive.getClass().equals(int.class)
-                || primitive.getClass().equals(Integer.class)) {
-            return ((Integer) primitive).toString();
-        } else if (primitive.getClass().equals(long.class)
-                || primitive.getClass().equals(Long.class)) {
-            return ((Long) primitive).toString();
-        } else if (primitive.getClass().equals(float.class)
-                || primitive.getClass().equals(Float.class)) {
-            return primitive.toString();
-        } else if (primitive.getClass().equals(double.class)
-                || primitive.getClass().equals(Double.class)) {
+                || primitive.getClass().equals(short.class)
+                || primitive.getClass().equals(int.class)
+                || primitive.getClass().equals(long.class)
+                || primitive.getClass().equals(float.class)
+                || primitive.getClass().equals(double.class)
+                || Number.class.isAssignableFrom(primitive.getClass())) {
             return primitive.toString();
         } else if (primitive.getClass().equals(char.class)
                 || primitive.getClass().equals(Character.class)) {

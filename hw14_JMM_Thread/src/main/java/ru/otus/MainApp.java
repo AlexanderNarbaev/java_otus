e67bcd36e391559e8c418b5720e25bb1d83926de
Alex_Foully <alexander.narbayev@gmail.com>
2020-05-24 22:50:07 +0700

    HomeWork14 - JMM and Thread

--- hw14_JMM_Thread/src/main/java/ru/otus/MainApp.java
+++ hw14_JMM_Thread/src/main/java/ru/otus/MainApp.java
@@ -1,78 +1,49 @@
 package ru.otus;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainApp {
+    private final Object monitor = new Object();
+    private boolean isRunning = true;
+    private boolean reverse = false;
+    private boolean printed = true;
+    private int count = 0;
 
-    private static final AtomicBoolean isRunning = new AtomicBoolean(true);
-    private static final AtomicInteger count = new AtomicInteger(1);
-    private static final AtomicBoolean reverse = new AtomicBoolean(false);
-    private static final AtomicBoolean printed = new AtomicBoolean(false);
-
-    public static void main(String[] args) throws InterruptedException {
-        Thread first = new Thread(new NumberWriter(reverse, count, isRunning, System.out, printed));
-        Thread second = new Thread(new NumberWriter(reverse, count, isRunning, System.err, printed));
-        first.start();
-        second.start();
-        Thread.sleep(TimeUnit.SECONDS.toMillis(20));
-        isRunning.set(false);
-    }
-}
-
-class NumberWriter implements Runnable {
-    private final AtomicBoolean reverse;
-    private final AtomicInteger count;
-    private final AtomicBoolean isRunning;
-    private final OutputStream outputStream;
-    private AtomicBoolean printed;
-
-    NumberWriter(AtomicBoolean reverse, AtomicInteger count, AtomicBoolean isRunning, OutputStream outputStream, AtomicBoolean printed) {
-        this.reverse = reverse;
-        this.count = count;
-        this.isRunning = isRunning;
-        this.outputStream = outputStream;
-        this.printed = printed;
-    }
-
-    @Override
-    public void run() {
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream))) {
-            while (isRunning.get()) {
-                writer.write(Thread.currentThread().getName());
-                writer.write(": ");
-                reverse.getAndSet(!reverse.get());
-                if (printed.get()) {
-                    writer.write(String.valueOf(reverse.get() ? count.decrementAndGet() : count.incrementAndGet()));
-                    printed.set(false);
+    public void printCount() {
+        while (isRunning) {
+            synchronized (monitor) {
+                if (printed) {
+                    System.out.println(Thread.currentThread().getName() + ": " + (reverse ? --count : ++count));
+                    printed = false;
                 } else {
-                    writer.write(String.valueOf(count.get()));
-                    printed.set(true);//Я так понимаю, это HappensBefore ребро
+                    System.out.println(Thread.currentThread().getName() + ": " + count);
+                    printed = true;
                 }
-
-                writer.write(" ");
-                writer.newLine();
-                writer.flush();
-                if (count.get() >= 10) {
-                    reverse.set(false);
+                if (count >= 10) {
+                    reverse = true;
                 }
-                if (count.get() <= 1) {
-                    reverse.set(true);
-                }
-                try {
-                    Thread.sleep(TimeUnit.MILLISECONDS.toMillis(ThreadLocalRandom.current().nextInt(100, 600)));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                if (count <= 1) {
+                    reverse = false;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextInt(400, 800));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MainApp app = new MainApp();
+        Thread t1 = new Thread(app::printCount);
+        Thread t2 = new Thread(app::printCount);
+        t1.setName("Thread_ONE");
+        t2.setName("Thread_TWO");
+        t1.start();
+        t2.start();
+        Thread.sleep(TimeUnit.SECONDS.toMillis(10));
+        app.isRunning = false;
     }
 }
